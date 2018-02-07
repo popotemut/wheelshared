44f462697076dba6aeae2954bf8f322d6829b836
popotemut <popo.temut41@gmail.com>
2018-02-07 12:12:58 +0700

    Deploy Successful version

--- src/main/java/model/Vocabulary.java
+++ src/main/java/model/Vocabulary.java
@@ -30,35 +30,39 @@ public class Vocabulary {
     
     public Connection connectionBuilder() throws SQLException, ClassNotFoundException{
         Properties p = new Properties();
-        p.setProperty("URL", "jdbc:mysql://localhost:3306/greetingdb?zeroDateTimeBehavior=convertToNull");
-        p.setProperty("USERNAME","root");
-        p.setProperty("PASSWORD","1234");
+        p.setProperty("URL", "jdbc:mysql://wheelshared.cb93cu4k2yln.us-west-2.rds.amazonaws.com:3306/wheelshared");
+        p.setProperty("USERNAME","adminwheelshared");
+        p.setProperty("PASSWORD","letsgoinside");
         
         Connection connection = null;
 
         // the mysql driver string
         Class.forName("com.mysql.jdbc.Driver");
-
+        System.out.println("after com.mysql.jdbc.Driver");
         // the mysql url
 
         // get the mysql database connection
-        connection = DriverManager.getConnection(p.getProperty("URL"),p.getProperty("USERNAME"), p.getProperty("PASSWORD"));
-
+        connection = DriverManager.getConnection(p.getProperty("URL")+"?user="+p.getProperty("USERNAME")+"&password="+p.getProperty("PASSWORD"));
+        
         // now do whatever you want to do with the connection
         // ...
         return connection;
     }
     
     public String getVocab(Connection con) throws SQLException{
+//    public String getVocab() throws SQLException{
                 Statement statement = con.createStatement();
+                System.out.println("after con.createStatement()");
         // Result set get the result of the SQL query
-        ResultSet resultSet = statement.executeQuery("select vocabulary from GREETINGDB.VOCABULARY;");
+        ResultSet resultSet = statement.executeQuery("select greeting from wheelshared.greeting;");
         String vocab = new String();
+        System.out.println("after statement.executeQuery");
         int i=0;
         while (resultSet.next()) {
-            vocab = resultSet.getString("VOCABULARY");
+            vocab = resultSet.getString("greeting");
             System.out.println("Vocaburary: " + vocab);
         }
+        con.close();
         return vocab;
     }
     
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -18,6 +18,8 @@
         <h1><%
             Vocabulary v = new Vocabulary();
             String s = v.getVocab(v.connectionBuilder());
+            
+//            String s = v.getVocab();
             out.println(s);
             %></h1>
     </body>
